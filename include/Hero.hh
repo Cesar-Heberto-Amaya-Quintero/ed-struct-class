613c0e7acbadd8b46679d52f88783bc636d7536b
Cesar-Heberto-Amaya-Quintero <amaya.cesar.cbtis37@gmail.com>
2020-09-06 16:58:29 -0700

    actualizado

---
+++ include/Hero.hh
@@ -0,0 +1,13 @@
+#include <Character.hh>
+
+class Hero : public Character
+{
+private:
+    
+public:
+    Hero(short int health, uint32_t strength, uint32_t exp, short int luck, uint32_t speed); //constructor
+    ~Hero(); //destructor
+};
+
+
+
